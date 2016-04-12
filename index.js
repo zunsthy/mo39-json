4b5938086f85580cd918be9a5ebe5a74db01cf7e
ylxdzsw <ylxdzsw@gmail.com>
2016-04-12 12:53:03 +0800

    fix a bug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict'
-const fs = require('fs');
+const fs   = require('fs');
+const path = require('path');
 
-module.exports = eval(fs.readFileSync('lib/mo39.js', 'utf8'));
+module.exports = eval(fs.readFileSync(path.join(__dirname, 'lib/mo39.js'), 'utf8'));
 
